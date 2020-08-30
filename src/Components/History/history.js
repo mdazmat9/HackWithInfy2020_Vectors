871e11a49a7a42a72f534696f2973cda8df821e7
mdazmat9 <38210242+mdazmat9@users.noreply.github.com>
2020-08-30 14:18:30 +0530

    added map

--- src/Components/History/history.js
+++ src/Components/History/history.js
@@ -1,12 +1,33 @@
 import React,{Component} from 'react'
 import {Link} from 'react-router-dom'
-
+import cx from 'classnames'
+import styles from '../Dashboard/dashboard.module.css'
 class history extends Component{
     render(){
         return(
             <>
-            HISTORY WIIL BE HERE
-            <Link to='/dashboard'>Back</Link>
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top  navbar-dark bg-dark">
+				<a className="navbar-brand" href="#">Where is my Truck</a>
+				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ml-auto">
+					<li className="nav-item ">
+						<Link to='/dashboard'><a className="nav-link" href="#">HOME</a></Link>
+					</li>
+					<li className="nav-item active">
+						<Link to='/history'><a className="nav-link" href="#">HISTORY<span className="sr-only">(current)</span></a></Link>
+					</li>
+					
+					<li>
+						<button className={cx("btn  btn-secondary")}>START</button>
+					</li>
+					
+					</ul>
+				</div>
+				</nav>
             </>
         )
     }
--- src/Components/Mainpage/mainpage.js
+++ src/Components/Mainpage/mainpage.js
@@ -4,12 +4,11 @@ import styles from './Mainpage.module.css'
 
 
 
-
 class Mainpage extends Component{
 	render(){
 		return(
 			<div className={styles.pagebody}>
-				<h1 className={styles.getStarted}>Track vehicle</h1>
+				<h1 className={styles.getStarted}>Track Vehicle</h1>
 				<Link className={styles.butttons} to='/dashboard'>
 					<button  type="button" class="btn btn-dark btn-lg">Get Started</button>
 				</Link>
